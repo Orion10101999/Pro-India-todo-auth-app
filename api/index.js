a65501bf2deb0b5b72d4755b7b0ca0796c27cd2c
Orion10101999 <orion10101999@gmail.com>
2024-08-31 00:12:20 +0530

    ok

---
+++ api/index.js
@@ -0,0 +1,45 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import path from 'path'
+import connectDB from './config/connectDB.js';
+import router from './routes/index.js';
+import todoRouter from './routes/todo.route.js';
+connectDB().then(() => {
+    console.log('mongodb atlas connected');
+
+    app.listen(PORT, () => {
+        console.log('server running at ' + PORT);
+    });
+}).catch((err) => {
+    console.log('mongodb atlas connection failed');
+});
+
+const __dirname = path.resolve();
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 8080;
+
+// api endpoints
+app.use('/api', router);
+app.use('/api/todo', todoRouter);
+
